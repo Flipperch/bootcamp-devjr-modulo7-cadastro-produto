fb3732540aa55fe44d5959a1dc5f84f0dc6b3541
Flipperch <flipperch@gmail.com>
2025-09-11 22:08:03 -0300

    Init repo

---
+++ product-backend/src/main/java/com/flipperch/product_backend/ProductBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flipperch.product_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductBackendApplication.class, args);
+	}
+
+}
